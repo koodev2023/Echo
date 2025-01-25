7c7dfb0a13b759aea3a4147ec278ce827c5f41bc
koodev2023 <koodev2023@gmail.com>
2025-01-25 14:22:26 +0800

    chore: removed post status tag

--- src/components/custom/CardDateViewCommentCount.tsx
+++ src/components/custom/CardDateViewCommentCount.tsx
@@ -25,26 +25,6 @@ const CardDateViewCommentCount = ({
         <MessageCircle className="w-4 h-4" />
         <div>{commentCount}</div>
       </div>
-
-      <div
-        className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
-          isPublished
-            ? "bg-green-100 text-green-800 ring-green-600/20"
-            : "bg-yellow-100 text-yellow-800 ring-yellow-600/20"
-        }`}
-      >
-        {isPublished ? "Published" : "Draft"}
-      </div>
-
-      {/* <div
-        className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
-          cardType === "VIEW"
-            ? "bg-green-100 text-green-800 ring-green-600/20"
-            : "bg-yellow-100 text-yellow-800 ring-yellow-600/20"
-        }`}
-      >
-        {cardType === "EDIT" ? "EDIT" : "VIEW"}
-      </div> */}
     </div>
   );
 };
